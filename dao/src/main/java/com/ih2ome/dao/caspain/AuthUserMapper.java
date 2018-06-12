55afb73d4c8442fa50faf9240bc3c602aefb9755
sky李齐 <sky.li@ixiaoshuidi.com>
2018-06-12 17:23:32 +0800

    金融

---
+++ dao/src/main/java/com/ih2ome/dao/caspain/AuthUserMapper.java
@@ -0,0 +1,32 @@
+package com.ih2ome.dao.caspain;
+
+import com.ih2ome.model.caspain.AuthUser;
+import com.ih2ome.model.caspain.AuthUserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Repository
+public interface AuthUserMapper {
+    int countByExample(AuthUserExample example);
+
+    int deleteByExample(AuthUserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(AuthUser record);
+
+    int insertSelective(AuthUser record);
+
+    List<AuthUser> selectByExample(AuthUserExample example);
+
+    AuthUser selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") AuthUser record, @Param("example") AuthUserExample example);
+
+    int updateByExample(@Param("record") AuthUser record, @Param("example") AuthUserExample example);
+
+    int updateByPrimaryKeySelective(AuthUser record);
+
+    int updateByPrimaryKey(AuthUser record);
+}
