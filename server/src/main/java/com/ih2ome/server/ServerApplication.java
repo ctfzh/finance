657b8c802c3925725e1e2e5d53c930becd391473
sky李齐 <sky.li@ixiaoshuidi.com>
2018-07-17 12:52:52 +0800

    properties 配置文件处理

--- server/src/main/java/com/ih2ome/server/ServerApplication.java
+++ server/src/main/java/com/ih2ome/server/ServerApplication.java
@@ -1,18 +1,24 @@
 package com.ih2ome.server;
 
+import com.ih2ome.server.config.MyServletContextListener;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 
 @SpringBootApplication
 @EnableTransactionManagement
 @ComponentScan(basePackages = {"com.ih2ome"})
+@ServletComponentScan
 public class ServerApplication extends SpringBootServletInitializer {
 
     @Override
--- server/src/main/java/com/ih2ome/server/config/MyServletContextListener.java
+++ server/src/main/java/com/ih2ome/server/config/MyServletContextListener.java
@@ -1,22 +1,21 @@
 package com.ih2ome.server.config;
 
 import com.ih2ome.common.pingan.sdk.InitConfiguration;
-import com.ih2ome.server.controller.ConfigPaymentsController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
 /**
  * @author Sky
- * create 2018/06/19
+ * create 2018/07/17
  * email sky.li@ixiaoshuidi.com
  **/
 @Component
-public class MyCommandLineRunner implements CommandLineRunner {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(MyCommandLineRunner.class);
+@WebListener
+public class MyServletContextListener implements ServletContextListener {
 
     //.cer开放平台证书(公钥)
     @Value("${pinganSDK.cerName}")
@@ -31,9 +30,14 @@ public class MyCommandLineRunner implements CommandLineRunner {
     private String protocolName;
 
     @Override
-    public void run(String... var1) throws Exception {
+    public void contextInitialized(ServletContextEvent sce) {
         //SDK的初始化操作
-        LOGGER.info("=====================SDK的初始化操作==MyCommandLineRunner");
+        System.out.println("SDK的初始化操作");
         InitConfiguration.init(cerName, pfxName, protocolName);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
     }
 }
