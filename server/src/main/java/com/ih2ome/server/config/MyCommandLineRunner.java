e6559a0f8022ae221be8ac61a763aa88dce7fef2
sky李齐 <sky.li@ixiaoshuidi.com>
2018-07-17 11:24:44 +0800

    properties 配置文件处理

--- server/src/main/java/com/ih2ome/server/config/MyCommandLineRunner.java
+++ server/src/main/java/com/ih2ome/server/config/MyCommandLineRunner.java
@@ -1,6 +1,9 @@
 package com.ih2ome.server.config;
 
 import com.ih2ome.common.pingan.sdk.InitConfiguration;
+import com.ih2ome.server.controller.ConfigPaymentsController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,6 +15,8 @@ import org.springframework.stereotype.Component;
  **/
 @Component
 public class MyCommandLineRunner implements CommandLineRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyCommandLineRunner.class);
 
     //.cer开放平台证书(公钥)
     @Value("${pinganSDK.cerName}")
@@ -28,6 +33,7 @@ public class MyCommandLineRunner implements CommandLineRunner {
     @Override
     public void run(String... var1) throws Exception {
         //SDK的初始化操作
+        LOGGER.info("=====================SDK的初始化操作==MyCommandLineRunner");
         InitConfiguration.init(cerName, pfxName, protocolName);
     }
 }
