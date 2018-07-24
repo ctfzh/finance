5b999264e3e2021e18cc24327c754a585ba7cb4e
sky李齐 <sky.li@ixiaoshuidi.com>
2018-07-24 17:54:31 +0800

    RandomUtil.java工具类修改

---
+++ common/src/main/java/com/ih2ome/common/PageVO/PinganWxRequestVO.java
@@ -0,0 +1,20 @@
+package com.ih2ome.common.PageVO;
+
+import lombok.Data;
+
+/**
+ * @author Sky
+ * create 2018/07/24
+ * email sky.li@ixiaoshuidi.com
+ **/
+@Data
+public class PinganWxRequestVO {
+    //商户门店open_id
+    private String open_id;
+    //Unix时间戳
+    private Long timestamp;
+    //签名
+    private String sign;
+    //用户提交的post请求数据
+    private String data;
+}
