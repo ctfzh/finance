eda9a0ab1f6fccb23e6291ef5475773268214c44
sky李齐 <sky.li@ixiaoshuidi.com>
2018-08-03 14:23:23 +0800

    支付子订单信息

---
+++ dao/src/main/java/com/ih2ome/model/lijiang/SubPayOrders.java
@@ -0,0 +1,30 @@
+package com.ih2ome.model.lijiang;
+
+import lombok.Data;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "sub_pay_orders")
+public class SubPayOrders {
+    @Id
+    @Column(name = "uuid")
+    private String uuid;
+    @Column(name = "order_id")
+    private String orderId;
+    @Column(name = "sub_order_id")
+    private String subOrderId;
+    @Column(name = "sub_account")
+    private String subAccount;
+    @Column(name = "tran_fee")
+    private Double tranFee;
+    @Column(name = "sub_amount")
+    private Double subAmount;
+    @Column(name = "remark")
+    private String remark;
+    @Column(name = "raw_data")
+    private String rawData;
+
+}
