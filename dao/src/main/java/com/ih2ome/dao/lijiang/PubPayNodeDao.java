be090ff2f7c6839b749689b2eb0ad57132ef0c20
sky李齐 <sky.li@ixiaoshuidi.com>
2018-08-13 15:19:17 +0800

    选择银行

---
+++ dao/src/main/java/com/ih2ome/dao/lijiang/PubPayNodeDao.java
@@ -0,0 +1,14 @@
+package com.ih2ome.dao.lijiang;
+
+import com.ih2ome.dao.BaseDao;
+import com.ih2ome.model.lijiang.PubPayNode;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Sky
+ * create 2018/08/13
+ * email sky.li@ixiaoshuidi.com
+ **/
+@Repository
+public interface PubPayNodeDao extends BaseDao<PubPayNode> {
+}
